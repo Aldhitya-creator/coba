58b4cd2cb55ade37a32df9cd8f424c6d65f6b992
Aldhitya-creator <alditya676@gmail.com>
2021-01-31 15:09:20 +0700

    ini di rubah ya htmlnya

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  module.exports = {
+    darkMode: 'media',
+    // ...
+  }
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
